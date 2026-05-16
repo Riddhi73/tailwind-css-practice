83022ca1ba23dfc89c8d94980a36d6bf62ae142f
Riddhi73 <riddhimandebnath4260@gmail.com>
2026-05-16 11:59:09 +0600

    Add background image

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
         <p className="text-blue-500">Thomas</p>
       </div>
       <div className="bg-black text-white">Tailwind is great</div>
+      <div className="bg-[url('https://tse4.mm.bing.net/th/id/OIP.NGIDdVP6vw9ue_D-mrEVFQHaHa?rs=1&pid=ImgDetMain&o=7&rm=3')] bg-no-repeat bg-cover h-screen"></div>
     </>
   );
 }
