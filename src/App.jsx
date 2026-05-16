4e546f1dd1e19b11c79965cadd2533795d21a834
Riddhi73 <riddhimandebnath4260@gmail.com>
2026-05-16 14:10:46 +0600

    Add breakpoint responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
 function App() {
   return (
     <>
-      <div className="flex bg-blue-500">
-        <div className="flex-1 border border-yellow-500">First Div 1</div>
-        <div className="flex-2 border border-pink-700">Second Div 2</div>
-      </div>
-      <div className="flex flex-col justify-center items-center">
-        <div>First</div>
-        <div>Second</div>
-      </div>
-      <div className="grid grid-cols-3 gap-4">
-        <div className="col-span-2">First</div>
-        <div>Second</div>
+      <div className="bg-blue-300 sm:bg-red-500 md:bg-blue-600 lg:bg-purple-600 xl:bg-yellow-400">
+        We are testing responsiveness
       </div>
     </>
   );
