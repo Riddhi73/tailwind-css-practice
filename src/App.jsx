3f706e10001a7636784026413822d8f4ef4d8e0b
Riddhi73 <riddhimandebnath4260@gmail.com>
2026-05-16 10:08:06 +0600

    Add tailwind css config

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <div className="text-3xl font-bold underline">
+        <h1>Hello world!</h1>
+      </div>
+      <div>
+        <p className="text-red-500">Tom</p>
+        <p className="text-gray-500 text-center">Jerry</p>
+        <p className="text-blue-500">Thomas</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
