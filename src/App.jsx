ee5839a3a6caf945786e92c4420540681bd05c6d
Riddhi73 <riddhimandebnath4260@gmail.com>
2026-05-16 13:16:14 +0600

    Add height width

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ function App() {
         <p className="text-blue-500">Thomas</p>
       </div>
       <div className="bg-black text-white m-4 p-8">Tailwind is great</div>
+      <div className="bg-blue-800 text-white w-1/2 h-2/3">
+        Tailwind is great
+      </div>
       <div className="bg-gradient-to-r from-blue-300 via-purple-300 to-pink-300">
         we are learning
       </div>
