ce5db77538e5734765359461727640122649df39
Riddhi73 <riddhimandebnath4260@gmail.com>
2026-05-16 12:47:15 +0600

    Add margin padding

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
         <p className="text-gray-500 text-center">Jerry</p>
         <p className="text-blue-500">Thomas</p>
       </div>
-      <div className="bg-black text-white">Tailwind is great</div>
+      <div className="bg-black text-white m-4 p-8">Tailwind is great</div>
       <div className="bg-gradient-to-r from-blue-300 via-purple-300 to-pink-300">
         we are learning
       </div>
