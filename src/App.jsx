ce2dcce9d84d03ca2346365324126a3c900db884
Riddhi73 <riddhimandebnath4260@gmail.com>
2026-05-16 14:43:44 +0600

    Add custom color

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ function App() {
       <div className="bg-blue-300 sm:bg-red-500 md:bg-blue-600 lg:bg-purple-600 xl:bg-yellow-400">
         We are testing responsiveness
       </div>
+      <div className="bg-primary text-customcolor-500">
+        I am testing custom colors
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#ff5622",
+        secondary: "#212565",
+        customcolor: {
+          100: "#259855",
+          500: "#850797",
+          800: "#123485",
+        },
+      },
+    },
   },
   plugins: [],
 };
